600d597d8420acac871a93cbc2db69c0f410d2ac
yusufneeson <yusufneeson@gmail.com>
2025-12-09 13:27:30 +0700

    style update background gradient

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -38,7 +38,7 @@ export default async function RootLayout({
       >
         <Providers user={user}>
           <div className="bg-gray-50 w-full h-full flex justify-center items-center">
-            <div className="bg-linear-to-r from-green-50 from-10% via-lime-100 via-60% to-emerald-200/50 min-w-[450px] min-h-screen border-green-100 border shadow-xl rounded-xl py-4 px-4">
+            <div className="bg-linear-to-br from-green-50 from-10% via-lime-100 via-50% to-emerald-200/50 min-w-[450px] min-h-screen border-green-100 border shadow-xl rounded-xl py-4 px-4">
               {children}
             </div>
           </div>
