37269c947824c6827216edd8c89d98bbdc3bc246
yusufneeson <yusufneeson@gmail.com>
2025-12-09 12:17:24 +0700

    Dashboard component: update look

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,22 +1,36 @@
 "use client";
 
 import { UserContext } from "@RSV/lib/user-context";
+import { BellDot } from "lucide-react";
 import { useContext } from "react";
+import BalanceCard from "./BalanceCard";
 
 export default function Dashboard() {
   const user = useContext(UserContext);
 
+  const accountBalance = {
+    balance: 89500250,
+    income: 21432467,
+    expense: 31862652
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold">Dashboard</h1>
-      {user ? (
-        <div>
-          <p>Welcome, {user.name}!</p>
-          <p>Email: {user.email}</p>
+    <div>
+      <div className="flex mb-5 justify-between">
+        <div className="flex gap-1 items-center">
+          <div className="bg-green-600 w-6 h-6 rounded-full"></div>
+          <span>{user?.name}</span>
         </div>
-      ) : (
-        <p>Please log in to view your dashboard.</p>
-      )}
+        <div>
+          <BellDot />
+        </div>
+      </div>
+
+      <BalanceCard
+        balance={accountBalance.balance}
+        income={accountBalance.income}
+        expense={accountBalance.expense}
+      />
     </div>
   );
 }
