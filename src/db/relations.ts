71f54264aec30014c0041bb691f4a1b94f39e231
Yusuf Neeson <yusufneeson@gmail.com>
2025-12-10 07:58:25 +0700

    update relations

--- src/db/relations.ts
+++ src/db/relations.ts
@@ -4,11 +4,42 @@ import * as schema from "./schema";
 export const relations = defineRelations(schema, (r) => ({
   users: {
     session: r.many.sessions(),
+    accounts: r.many.accounts(),
+    categories: r.many.categories(),
+    transactions: r.many.transactions(),
   },
   sessions: {
     user: r.one.users({
       from: r.sessions.userId,
       to: r.users.id,
+    }),
+  },
+  accounts: {
+    user: r.one.users({
+      from: r.accounts.userId,
+      to: r.users.id,
+    }),
+    transactions: r.many.transactions(),
+  },
+  categories: {
+    user: r.one.users({
+      from: r.categories.userId,
+      to: r.users.id,
+    }),
+    transactions: r.many.transactions(),
+  },
+  transactions: {
+    user: r.one.users({
+      from: r.transactions.userId,
+      to: r.users.id,
+    }),
+    accounts: r.one.accounts({
+      from: r.transactions.accountId,
+      to: r.accounts.id,
+    }),
+    categories: r.one.categories({
+      from: r.transactions.categoryId,
+      to: r.categories.id,
     }),
   },
 }));
