4d1ffefe1aa005b9313a5139d5d35f68462dcc6f
yusufneeson <yusufneeson@gmail.com>
2025-12-11 18:36:02 +0700

    get transactions from server component

--- src/app/(authenticated)/page.tsx
+++ src/app/(authenticated)/page.tsx
@@ -1,12 +1,14 @@
 import Dashboard from "@RSV/components/Dashboard";
 import { getAccounts } from "@RSV/lib/accounts";
 import { getUser } from "@RSV/lib/auth/get-user";
+import { getTransactions } from "@RSV/lib/transactions";
 
 export default async function Home() {
   const user = await getUser();
   const { accounts, balance } = await getAccounts(user.id);
+  const transactions = await getTransactions(user.id)
 
   return (
-      <Dashboard user={user} accounts={accounts} balance={balance} />
+      <Dashboard user={user} accounts={accounts} balance={balance} transactions={transactions} />
   );
 }
