c53a73533db4b5d414bec637f1d8ae66b40c045b
yusufneeson <yusufneeson@gmail.com>
2025-12-13 15:55:27 +0700

    login page: fix google icon

--- src/app/(guest)/login/page.tsx
+++ src/app/(guest)/login/page.tsx
@@ -2,18 +2,18 @@ import Image from "next/image";
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-neutral-900">
       <a href="/api/auth/login">
-        <button className="bg-white rounded-md py-2 px-4 flex items-center gap-2 justify-center cursor-pointer">
+        <button className="bg-white dark:bg-neutral-800 border border-gray-300 dark:border-neutral-600 shadow-2xl rounded-md py-2 px-4 flex items-center gap-2 justify-center cursor-pointer">
           <div>
-            {/*<Image
-              src="/assets/google-icon.ico"
+            <Image
+              src="/assets/google-icon.png"
               width={24}
               height={24}
               alt="Google Logo"
-            />*/}
+            />
           </div>
-          <div className="text-black">Sign in with Google</div>
+          <div className="text-black dark:text-neutral-300">Sign in with Google</div>
         </button>
       </a>
     </div>
